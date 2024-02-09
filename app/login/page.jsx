bf3900caf0c374e6f9d34704b1f144df25ef6254
Feli87 <hckt87@gmail.com>
2024-02-09 04:39:09 -0300

    update

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import styles from '@/app/ui/login/loginpage.module.css';
+import styles from '../ui/login/loginpage.module.css';
 import { MdError } from 'react-icons/md';
 
 const Login = () => {
